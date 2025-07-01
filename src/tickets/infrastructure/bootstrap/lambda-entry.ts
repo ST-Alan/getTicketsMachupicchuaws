c917261a4b9f4758da66de6d4ac48083f60155ff
ST-Alan <contacto@alanfermin.com>
2025-06-30 20:59:14 -0500

    finally

--- src/tickets/infrastructure/bootstrap/lambda-entry.ts
+++ src/tickets/infrastructure/bootstrap/lambda-entry.ts
@@ -1,13 +1,4 @@
 // src/tickets/infrastructure/bootstrap/lambda-entry.ts
-
-// Polyfill para la Web Crypto API. Es importante que esté al principio.
-if (typeof globalThis.crypto === 'undefined') {
-  // eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-unsafe-assignment
-  const crypto = require('crypto');
-  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-member-access
-  (globalThis as any).crypto = crypto.webcrypto;
-}
-
 import 'reflect-metadata';
 
 export * from './App';
